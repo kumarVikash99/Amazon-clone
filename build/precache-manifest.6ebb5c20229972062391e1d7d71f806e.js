4c05859684fc83258b1580a40404ad284f05db0c
vikash <vkm762003@gmail.com>
2024-05-21 14:44:28 +0530

    Resolve merge conflicts

---
+++ build/precache-manifest.6ebb5c20229972062391e1d7d71f806e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c68819e5d0fc8ac3c9ca71cd8f1b0fb2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7a577333747708d1176a",
+    "url": "/static/css/main.25a8238a.chunk.css"
+  },
+  {
+    "revision": "3f2a2c8957d7508db459",
+    "url": "/static/js/2.f2001c9c.chunk.js"
+  },
+  {
+    "revision": "4e9e6a426e4934b119d62e02d6fa5a5c",
+    "url": "/static/js/2.f2001c9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a577333747708d1176a",
+    "url": "/static/js/main.13ac149d.chunk.js"
+  },
+  {
+    "revision": "871888e9f7f8a6d4a0ba",
+    "url": "/static/js/runtime-main.6ee639cd.js"
+  }
+]);
